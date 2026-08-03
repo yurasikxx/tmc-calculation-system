7f4f2f6884df4172190937a1ceadcb78447db99b
Yury Tkachyk <yurasikxx@gmail.com>
2026-08-03 17:46:33 +0300

    initial commit

---
+++ src/main/java/by/bsuir/tcs/TmcCalculationSystemApplication.java
@@ -0,0 +1,13 @@
+package by.bsuir.tcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmcCalculationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmcCalculationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/bsuir/tcs/TmcCalculationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package by.bsuir.tcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmcCalculationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
